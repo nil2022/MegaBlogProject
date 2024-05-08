5c5ca38ff212667a9dd742c9ee88c761fb328250
Nilanjan Haldar <nil.haldar@gmail.com>
2024-05-08 13:19:52 +0530

    fix opening pages in multiple tabs doesn't get logged out, keeps session even after closing browser

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { useDispatch } from 'react-redux'
 import authService from '../../appwrite/auth'
 import { logout } from '../../store/authSlice'
@@ -7,10 +6,18 @@ import { useNavigate } from 'react-router-dom'
 function LogoutBtn() {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const logoutHandler = () => {
-        authService.logout().then(() => {
+    const logoutHandler = async() => {
+        authService.logout()
+        .then(() => {
             console.log('Logout Successfull')
             alert('Logout Successfull')
+            localStorage.removeItem('userStatus')
+            dispatch(logout())
+            navigate('/login')
+        })
+        .catch((error) => {
+            console.log('Server Error: ', error)
+            alert('Session Expired ! Please Login Again')
             localStorage.removeItem('userStatus')
             dispatch(logout())
             navigate('/login')
